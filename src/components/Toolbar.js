95cd84002903b4ca0b9e65569cc71d782f455089
M4x1m3 <M4x1me@protonmail.com>
2020-03-08 15:34:23 +0100

    404

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -60,6 +60,7 @@ export default class Toolbar extends Component {
                 <div className="header__links">
                     <NavLink className="header__links__link" activeClassName="header__links__link-active" to="/install/latest" exact>Install</NavLink>
                     <NavLink className="header__links__link" activeClassName="header__links__link-active" to="/releases" exact>Releases</NavLink>
+                    <NavLink className="header__links__link" activeClassName="header__links__link-active" to="/simulator" exact>Simulator</NavLink>
                     <a className="header__links__link" href="https://github.com/Omega-Numworks/Omega" target="_blank" rel="noopener noreferrer">Github</a>
                     
                     <div className="header__links__separator" />
