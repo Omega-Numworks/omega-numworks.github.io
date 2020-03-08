c612b97c098383228e93534638bdaa1bf5af42be
Quentin Guidée <quentin.guidee@gmail.com>
2020-03-08 15:57:13 +0100

    [Fix] Error 422

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './sass/omega.library.sass'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Toolbar from './components/Toolbar';
