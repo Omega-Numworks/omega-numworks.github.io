ec9a843efc97ef31a992cc554c77a450aa9c071b
Quentin Guidée <quentin.guidee@gmail.com>
2020-07-18 19:38:00 +0200

    [Fix] Hide external popup when the calc connection is lost

--- src/pages/Install.js
+++ src/pages/Install.js
@@ -132,7 +132,8 @@ export default class Install extends Component {
         this.setState({
             calculatorDetected: false,
             osDetected: "",
-            install: false
+            install: false,
+            model: "NXXXX"
         })
         this.hideTags();
     }
