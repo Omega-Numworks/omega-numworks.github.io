6a89f3eec7617f176c6448cc34eeffa9d7c14446
M4x1m3 <M4x1me@protonmail.com>
2020-07-20 13:02:21 +0200

    [wiki] Menu (3rd try)

--- src/pages/Wiki.js
+++ src/pages/Wiki.js
@@ -10,10 +10,19 @@ export default class Wiki extends Component {
         
         this.state = {
             markdown: null,
-            isLoading: true
+            isLoading: true,
+            menuOpen: true
         };
         
-        document.title = "Omega - Wiki"
+        document.title = "Omega - Wiki";
+        
+        this.clickOnToggle = this.clickOnToggle.bind(this);
+    }
+    
+    clickOnToggle() {
+        this.setState({
+            menuOpen: !this.state.menuOpen
+        });
     }
     
     componentWillMount() {
@@ -38,35 +47,40 @@ export default class Wiki extends Component {
         return (
             <div className="content wiki">
                 <div className="wiki__sidebar">
-                    <div className="wiki__sidebar__supercategories">
-                        <div className="wiki__sidebar__supercategories__supercategory">
-                            <div className="wiki__sidebar__supercategories__supercategory__title">Software</div>
-                            <i className="wiki__sidebar__supercategories__supercategory__icon material-icons-round">text_snippet</i>
+                    <div className={"wiki__sidebar__content" + (this.state.menuOpen ? "" : " wiki__sidebar__content__hide")}>
+                        <div className="wiki__sidebar__content__supercategories">
+                            <div className="wiki__sidebar__content__supercategories__supercategory">
+                                <div className="wiki__sidebar__content__supercategories__supercategory__title">Software</div>
+                                <i className="wiki__sidebar__content__supercategories__supercategory__icon material-icons-round">text_snippet</i>
+                            </div>
+                            <div className="wiki__sidebar__content__supercategories__supercategory wiki__sidebar__content__supercategories__supercategory">
+                                <div className="wiki__sidebar__content__supercategories__supercategory__title">Hardware</div>
+                                <i className="wiki__sidebar__content__supercategories__supercategory__icon material-icons-round">memory</i>
+                            </div>
+                            <div className="wiki__sidebar__content__supercategories__supercategory wiki__sidebar__content__supercategories__supercategory-active">
+                                <div className="wiki__sidebar__content__supercategories__supercategory__title">Python</div>
+                                <i className="wiki__sidebar__content__supercategories__supercategory__icon material-icons-round">code</i>
+                            </div>
                         </div>
-                        <div className="wiki__sidebar__supercategories__supercategory wiki__sidebar__supercategories__supercategory">
-                            <div className="wiki__sidebar__supercategories__supercategory__title">Hardware</div>
-                            <i className="wiki__sidebar__supercategories__supercategory__icon material-icons-round">memory</i>
+                        <div className="wiki__sidebar__content__category">
+                            <div className="wiki__sidebar__content__category__title">
+                                Introduction
+                                <i className="wiki__sidebar__content__category__title__arrow material-icons-round">arrow_drop_up</i>
+                            </div>
+                            <div className="wiki__sidebar__content__category__content">
+                                <div className="wiki__sidebar__content__category__content__item">Première page</div>
+                                <div className="wiki__sidebar__content__category__content__item">Deuxième page</div>
+                            </div>
                         </div>
-                        <div className="wiki__sidebar__supercategories__supercategory wiki__sidebar__supercategories__supercategory-active">
-                            <div className="wiki__sidebar__supercategories__supercategory__title">Python</div>
-                            <i className="wiki__sidebar__supercategories__supercategory__icon material-icons-round">code</i>
+                        <div className="wiki__sidebar__content__category">
+                            <div className="wiki__sidebar__content__category__title">
+                                Build system
+                                <i className="wiki__sidebar__content__category__title__arrow material-icons-round">arrow_drop_down</i>
+                            </div>
                         </div>
                     </div>
-                    <div className="wiki__sidebar__category">
-                        <div className="wiki__sidebar__category__title">
-                            Introduction
-                            <i className="wiki__sidebar__category__title__arrow material-icons-round">arrow_drop_up</i>
-                        </div>
-                        <div className="wiki__sidebar__category__content">
-                            <div className="wiki__sidebar__category__content__item">Première page</div>
-                            <div className="wiki__sidebar__category__content__item">Deuxième page</div>
-                        </div>
-                    </div>
-                    <div className="wiki__sidebar__category">
-                        <div className="wiki__sidebar__category__title">
-                            Build system
-                            <i className="wiki__sidebar__category__title__arrow material-icons-round">arrow_drop_down</i>
-                        </div>
+                    <div className={"wiki__sidebar__toggle" + (this.state.menuOpen ? "" : " wiki__sidebar__toggle__enabled")} onClick={this.clickOnToggle}>
+                        <i className="material-icons-round wiki__sidebar__arrow">arrow_drop_down</i>
                     </div>
                 </div>
                 <div className="wiki__content">
