a7c7e1bc3201737209823668b00bfb91a4da5294
M4x1m3 <M4x1me@protonmail.com>
2020-08-03 20:44:47 +0200

    [ide] Finished Components

--- src/pages/IDE/Editor.js
+++ src/pages/IDE/Editor.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import { Redirect } from "react-router-dom";
 import GithubConnector from "../../GithubConnector";
-import MonacoEditor from 'react-monaco-editor';
-import ReactResizeDetector from 'react-resize-detector';
 
 import File from './components/File';
 import Project from './components/Project';
 import {LeftMenu, LeftMenuTitle, LeftMenuContent, LeftMenuActions, LeftMenuAction} from './components/LeftMenu';
 import {LeftBar, LeftBarTop, LeftBarBottom, LeftBarAction} from './components/LeftBar';
 import {BottomBar, BottomBarElement} from './components/BottomBar';
+import {Greeting, GreetingLogo, GreetingTitle, GreetingVersion, Help, HelpLine, HelpLeft, HelpRight, HelpKey} from './components/Greeting';
+import {TopBarTabs, TopBarMore, TopBarFileName, TopBarTab, TopBar} from './components/TopBar';
+import Monaco from './components/Monaco';
 import Loader from './components/Loader';
 
 export default class IDEEditor extends Component {
@@ -97,6 +98,36 @@ export default class IDEEditor extends Component {
         this.state.connector.removeAuthStateChanged(this.onAuthStateChanged);
     }
 
+    renderGreeting() {
+        return (
+            <Greeting>
+                <GreetingLogo />
+                <GreetingTitle>Omega IDE</GreetingTitle>
+                <GreetingVersion>2.0.0 Alpha</GreetingVersion>
+                <Help>
+                    <HelpLine>
+                        <HelpLeft>Create a new project</HelpLeft>
+                        <HelpRight>
+                            <HelpKey>Ctrl</HelpKey> + <HelpKey>N</HelpKey>
+                        </HelpRight>
+                    </HelpLine>
+                    <HelpLine>
+                        <HelpLeft>Run simulator</HelpLeft>
+                        <HelpRight>
+                            <HelpKey>F5</HelpKey>
+                        </HelpRight>
+                    </HelpLine>
+                    <HelpLine>
+                        <HelpLeft>Send to device</HelpLeft>
+                        <HelpRight>
+                            <HelpKey>F6</HelpKey>
+                        </HelpRight>
+                    </HelpLine>
+                </Help>
+            </Greeting>
+        );
+    }
+
     renderEditor() {
         return (
             <div className="editor">
@@ -136,108 +167,22 @@ export default class IDEEditor extends Component {
                     </LeftMenu>
 
                     <div className="editor__panel">
-                        {/* Greeting */}
-                        {/*
-                        <div className="editor__panel__greeting">
-                            <div className="editor__panel__greeting__content">
-                                <img className="editor__panel__greeting__content__logo" src={OmegaLogo} alt="Omega Logo"/>
-                                <h1 className="editor__panel__greeting__content__title">
-                                    Omega IDE
-                                </h1>
-                                <h2 className="editor__panel__greeting__content__version">
-                                    2.0.0 Alpha
-                                </h2>
-                                <div className="editor__panel__greeting__content__help">
-                                    <div className="editor__panel__greeting__content__help__line">
-                                        <div className="editor__panel__greeting__content__help__left">
-                                            Create a new project
-                                        </div>
-                                        <div className="editor__panel__greeting__content__help__right">
-                                        <span className="editor__panel__greeting__content__help__key">Ctrl</span>
-                                        +
-                                        <span className="editor__panel__greeting__content__help__key">N</span>
-                                        </div>
-                                    </div>
-                                    <div className="editor__panel__greeting__content__help__line">
-                                        <div className="editor__panel__greeting__content__help__left">
-                                            Run in simulator
-                                        </div>
-                                        <div className="editor__panel__greeting__content__help__right">
-                                        <span className="editor__panel__greeting__content__help__key">F5</span>
-                                        </div>
-                                    </div>
-                                    <div className="editor__panel__greeting__content__help__line">
-                                        <div className="editor__panel__greeting__content__help__left">
-                                            Send on calculator
-                                        </div>
-                                        <div className="editor__panel__greeting__content__help__right">
-                                        <span className="editor__panel__greeting__content__help__key">F6</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        */}
                         {/* Top Bar */}
-                        <div className="editor__panel__topbar">
-                            <div className="editor__panel__topbar__tabs">
-                                <div className="editor__panel__topbar__tabs__container">
-                                    <div className="editor__panel__topbar__tab">
-                                        <span className="editor__panel__topbar__tab__name">
-                                            lorem.py
-                                        </span>
-                                        <i className="editor__panel__topbar__tab__close editor__panel__topbar__tab__close-unsaved material-icons">close</i>
-                                    </div>
-                                    <div className="editor__panel__topbar__tab">
-                                        <span className="editor__panel__topbar__tab__name">
-                                            ipsum.py
-                                        </span>
-                                        <i className="editor__panel__topbar__tab__close material-icons">close</i>
-                                    </div>
-                                    <div className="editor__panel__topbar__tab editor__panel__topbar__tab-selected">
-                                        <span className="editor__panel__topbar__tab__name">
-                                            dolor.py
-                                        </span>
-                                        <i className="editor__panel__topbar__tab__close editor__panel__topbar__tab__close-unsaved material-icons">close</i>
-                                    </div>
-                                    <div className="editor__panel__topbar__tab">
-                                        <span className="editor__panel__topbar__tab__name">
-                                            sit_amet_aaa.py
-                                        </span>
-                                        <i className="editor__panel__topbar__tab__close material-icons">close</i>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="editor__panel__topbar__more">
-                                <i className="editor__panel__topbar__more__icon material-icons">more_horiz</i>
-                            </div>
-
-                            <div className="editor__panel__topbar__filename">
-                                <span className="editor__panel__topbar__filename__content">
-                                    Projet > fichier.py
-                                </span>
-                            </div>
-                        </div>
+                        <TopBar>
+                            <TopBarTabs>
+                                <TopBarTab>aaa.py</TopBarTab>
+                                <TopBarTab selected={true}>aaa.py</TopBarTab>
+                                <TopBarTab unsaved={true}>aaa.py</TopBarTab>
+                                <TopBarTab selected={true} unsaved={true}>aaa.py</TopBarTab>
+                            </TopBarTabs>
+                            <TopBarMore />
+                            <TopBarFileName>
+                                Projet > fichier.py
+                            </TopBarFileName>
+                        </TopBar>
 
                         {/* Monaco */}
-                        <div className="editor__panel__monaco">
-                            <ReactResizeDetector handleWidth handleHeight>
-                                <MonacoEditor
-                                    ref="monaco"
-                                    width="100%"
-                                    height="100%"
-                                    language="python"
-                                    theme="vs-dark"
-                                    value={""}
-                                />
-                                    {
-                                    //value={code}
-                                    //options={options}
-                                    //onChange={(nv, e) => this.onChange(nv, e)}
-                                    //editorDidMount={this.editorDidMount}
-                                    }
-                            </ReactResizeDetector>
-                        </div>
+                        <Monaco />
                     </div>
                 </div>
 
