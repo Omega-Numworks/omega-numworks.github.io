85c74d256320c2bf17cf07cb6f979220a856cc38
Quentin Guidée <quentin.guidee@gmail.com>
2021-04-15 17:45:19 +0200

    [firebase] Import firebase/storage

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
-import firebase from 'firebase/app';
-import 'firebase/auth'
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/storage";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDVGEyNqgLCCerqbqGmUQ3mMxu8M4sYZvo",
@@ -9,9 +10,8 @@ const firebaseConfig = {
     storageBucket: "omega-numworks.appspot.com",
     messagingSenderId: "172338146789",
     appId: "1:172338146789:web:3000e6cb87d21249c8530c",
-    measurementId: "G-P9YFFF08LN"
+    measurementId: "G-P9YFFF08LN",
 };
-
 
 firebase.initializeApp(firebaseConfig);
 
