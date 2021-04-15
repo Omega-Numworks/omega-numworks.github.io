83b2aa3027db7717acfc632a24171beaf6b353eb
Quentin Guidée <quentin.guidee@gmail.com>
2021-04-15 17:16:59 +0200

    [index] Remove service worker

--- src/index.js
+++ src/index.js
@@ -10,12 +10,3 @@ ReactDOM.render(<App/>, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 // serviceWorker.unregister();
-
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('../firebase-messaging-sw.js')
-  .then(function(registration) {
-    console.log('Registration successful, scope is:', registration.scope);
-  }).catch(function(err) {
-    console.log('Service worker registration failed, error:', err);
-  });
-}
