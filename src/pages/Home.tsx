118f7bd05abfc8ee8a6925b97bbffeced2781220
Quentin Guidée <quentin.guidee@gmail.com>
2022-03-01 15:20:41 +0100

    Random improvements (design/texts)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,11 +7,11 @@ import ImgRpn from "../img/rpn.png";
 import ImgRpnDark from "../img/rpn-dark.png";
 import Img3ds from "../img/3ds.png";
 import Img3dsDark from "../img/3ds-dark.png";
-import ImgBootloader from "../img/bootloader-light.png"
-import ImgBootloaderDark from "../img/bootloader-dark.png"
-import ImgPhi from "../img/phi-home.png"
-import OmegaNeonLogo from "../img/logo-neon.png"
-import ShrugIcon from "../img/shrug.png"
+import ImgBootloader from "../img/bootloader-light.png";
+import ImgBootloaderDark from "../img/bootloader-dark.png";
+import ImgPhi from "../img/phi-home.png";
+import OmegaNeonLogo from "../img/logo-neon.png";
+import ShrugIcon from "../img/shrug.png";
 import Button from "../components/button/Button";
 import { FormattedMessage } from "react-intl";
 import Fade from "react-reveal";
@@ -31,7 +31,11 @@ type HomeProps = {
 };
 
 function PhiURL() {
-    return <a className="phi-link" href="https://phi.getomega.dev">Phi</a>;
+    return (
+        <a className="phi-link" href="https://phi.getomega.dev">
+            Phi
+        </a>
+    );
 }
 
 export default class Home extends Component<HomeProps> {
@@ -47,7 +51,7 @@ export default class Home extends Component<HomeProps> {
                 <div
                     className="project-description"
                     style={{
-                        backgroundImage: `url(https://unsplash.com/photos/bMkRxaVMvj4/download?ixid=MnwxMjA3fDB8MXx0b3BpY3x8Ym84alFLVGFFMFl8fHx8fDJ8fDE2NDYwNzYyMjg&force=true&w=1920)`
+                        backgroundImage: `url(https://unsplash.com/photos/bMkRxaVMvj4/download?ixid=MnwxMjA3fDB8MXx0b3BpY3x8Ym84alFLVGFFMFl8fHx8fDJ8fDE2NDYwNzYyMjg&force=true&w=1920)`,
                     }}
                 >
                     <h1 className="project-description__title">
@@ -89,7 +93,6 @@ export default class Home extends Component<HomeProps> {
 
                 <div style={{ height: "16px" }}></div>
 
-
                 <Fade>
                     <FeatureCardRow className="featureCardRow">
                         <FeatureCardColumn>
@@ -99,10 +102,12 @@ export default class Home extends Component<HomeProps> {
                                         id="home.features.symbolic.name"
                                         defaultMessage="Symbolic computation"
                                     /> */}
-                                    Bootloader
+                                    Dual boot
                                 </FeatureCardTitle>
                                 <FeatureCardDescription>
-                                    Omega integrates a new Bootloader. It allows you to boot multiple OS from different slots.
+                                    Omega integrates a new Bootloader. It allows
+                                    you to boot multiple OS from different
+                                    slots.
                                     {/* <FormattedMessage
                                         id="home.features.symbolic.description"
                                         defaultMessage="Symbolic computation was removed from Epsilon in version 11.2. Omega reintroduces that feature."
@@ -132,7 +137,8 @@ export default class Home extends Component<HomeProps> {
                                         id="home.features.symbolic.description"
                                         defaultMessage="Symbolic computation was removed from Epsilon in version 11.2. Omega reintroduces that feature."
                                     /> */}
-                                    <PhiURL /> is a tool that unlocks your calculator after an Epsilon 16+ update.
+                                    <PhiURL /> is a tool that unlocks your
+                                    calculator after an Epsilon 16+ update.
                                 </FeatureCardDescription>
                                 <FeatureCardImage
                                     src={ImgPhi}
