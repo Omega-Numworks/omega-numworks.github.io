2126a0f6da7f198afe9230e03a2606dac6b728ee
Maxime <M4x1me@protonmail.com>
2023-02-18 20:06:57 +0100

    Updated latest firmware

--- src/firmware/firmwares.tsx
+++ src/firmware/firmwares.tsx
@@ -35,7 +35,7 @@ export interface Compatibility {
 export type Language = "en" | "fr" | "nl" | "pt" | "it" | "de" | "es" | "hu";
 
 export const releases: Releases = {
-    latest: "O2.0.4-E15",
+    latest: "O2.0.5-E15",
     firmwares: [
         {
             name: "O2.0.5-E15",
